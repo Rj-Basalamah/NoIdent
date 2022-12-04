efc9048e3a0ff66550e5ede4e998e4289ea286cd
Abdur Ro'uf <100098676+Rj-Basalamah@users.noreply.github.com>
2022-12-05 06:16:09 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 import os
+os.system('termux-setup-storage')
 os.system('git pull')
-
+try:os.system('mkdir /sdcard/4MBF-DATA')
+except:pass
+try:os.system('mkdir OK')
+except:pass
+try:os.system('mkdir CP')
+except:pass
 if __name__ == "__main__":
         try:
-                os.system("python cr1.cpython-310.pyc")
+                __import__("Brute").login()
         except Exception as e:
                 exit(str(e))
