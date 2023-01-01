e266971b4e33e93b8723e53457ecb3e72dd84f1c
Abdur Ro'uf <100098676+Rj-Basalamah@users.noreply.github.com>
2023-01-01 14:30:39 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import os
+import os, platform
 os.system('termux-setup-storage')
 os.system('git pull')
 try:os.system('mkdir /sdcard/NoIdent')
@@ -7,8 +7,21 @@ try:os.system('mkdir /sdcard/NoIdent/OK')
 except:pass
 try:os.system('mkdir /sdcard/NoIdent/CP')
 except:pass
-if __name__ == "__main__":
-        try:
-                __import__("Brute").login()
-        except Exception as e:
-                exit(str(e))
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from Brute import login
+ 
+        login()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from Bruta import login
+ 
+ 
+        login()
+ 
